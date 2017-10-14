5b9f3df260d7514321b432d6d281e0b02444e96c
Lindsay <lindsaye@3436c316364l.corp.espn.pvt>
2017-10-14 18:18:34 -0400

    css

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,13 +2,45 @@ import React from 'react';
 
 const Article = props => (
   <div className="article">
-    <h1 className="article-title">This is an article!</h1>
+
+    <h1 className="article-title">In California, Fires So Fast Hesitation Proved Lethal</h1>
+    <h3 className="author">By <span className="author-name"> THOMAS FULLER</span>, <span className="author-name">Brian Jalokie</span> and <span className="author-name">RICHARD PÉREZ-PEÑA</span></h3>
     <p className="article-body">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      SANTA ROSA, Calif. — With towering flames bearing down, one victim delayed escape in hopes of saving his new truck — but he could not find the keys. An elderly couple slept as danger erupted, not waking until it was too late to flee down their one-lane road. Another couple, who barely missed their chance to drive away, huddled in a pool, surrounded by fire and choking smoke; he survived, but she did not.
+
+As widely varied stories emerged of how people died in the wind-driven fires that have ravaged Northern California, the element common to each tragedy — and to many of the tales of people who got out alive — was how quickly it happened. Advance warning was measured in minutes or seconds, or never came at all. Hesitation was lethal.
+
+“My dad’s best friend was calling and calling my parents, but they were completely asleep,” said Trina Grant, whose parents, Arthur and Suiko Grant, died at their hilltop property just outside Santa Rosa. “By the time my dad finally picked up and his friend said ‘You’ve got to get out,’ it was probably already too late.”
+
+The confirmed death toll reached 36 on Saturday, making this the deadliest wildfire outbreak in California history, and the figure is likely to climb. Thousands of homes and businesses have been destroyed, hundreds of people who have been reported missing remain unaccounted for, and emergency workers have barely begun the grim work of combing through the blackened, smoking ruins of houses, cars, forests and businesses.
+
+On Sunday night and Monday, 50-mile-per-hour winds propelled the fires faster than people could run. Embers leapfrogged hundreds of yards, even jumping across Highway 101, six lanes wide, which ordinarily would have served as an ideal, built-in fire break.
+
+Continue reading the main story
+RELATED COVERAGE
+
+graphic
+Northern California Fires Have Destroyed at Least 5,700 Buildings OCT. 14, 2017
+
+California Fires Leave 32 Dead, a Vast Landscape Charred, and a Sky Full of Soot OCT. 12, 2017
+
+California’s Wildfires: Why Have They Been So Destructive? OCT. 11, 2017
+
+‘Thank God We Left’: Californians Describe Escaping Wildfires OCT. 11, 2017
+
+Opinion Op-Ed Contributor
+In California’s Wine Country, Reckoning With an Inferno OCT. 13, 2017
+Search and rescue teams from around Northern California converged on the incinerated remains of Journey’s End on Friday, a mobile home park for seniors in Santa Rosa. They came with shovels, rakes and a cadaver-sniffing Australian cattle dog named Seven.
+
+They left with bone fragments.
+
+These were among the first steps in a painstaking process of finding the missing and the dead, under unusually difficult conditions. The fires were so hot that car wheels made of aluminum — which has a melting point of 1,221 degrees Fahrenheit — turned into puddles.
+
+“People have been cremated, for lack of a better term,” said Sgt. Ray Kelly, a spokesman for the Alameda County Sheriff’s Office, which has more than 40 experts helping in the forensic investigation. “Any DNA that was on the bones is gone — it’s consumed,” Sergeant Kelly said.
+
+At Journey’s End, technicians in bright orange and lemon yellow jackets raked through ash and fragments of household items. In the debris were the frame of a bed, the wire covering of a fan and magazines and books turned to ash that disintegrated when touched.
+
+“This will be without a doubt the most complicated identification process this area has ever seen,” said Jim Wood, an expert in forensic dentistry who was helping identify bodies through dental records. Mr. Wood, who now represents the area in the State Assembly, also helped identify victims of the Sept. 11 attacks.
     </p>
   </div>
 );
