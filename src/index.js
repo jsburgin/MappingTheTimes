c8946cbbeb543c3e1cb7d2177dae236871c588e1
Sidney Packer <sidp@knights.ucf.edu>
2017-10-14 15:50:41 -0400

    Start setting up map, header, and AL components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './App';
+import Wrapper from './containers/Wrapper';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Wrapper />, document.getElementById('root'));
 registerServiceWorker();
