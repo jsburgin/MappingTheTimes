df028e8b36b538be6c2b42063aad0f997967c792
Joshua Burgin <jsburgin@crimson.ua.edu>
2017-10-14 22:11:43 -0400

    added api route for geocoding

--- backend/routes/routers/api.js
+++ backend/routes/routers/api.js
@@ -2,14 +2,22 @@ const express = require('express');
 const http = require('../../middleware/http');
 const api = require('../../api');
 const natlang = require('../../natlang');
+const mapbox = require('../../services/mapbox');
 
 const router = express.Router();
 
 router.get('/articles', http.wrap(api.articles.get));
 router.get('/keys', http.wrap(api.keys.get));
+
 router.get('/process', (req, res, next) => {
   natlang.processUrl(req.query.url)
     .then(data => res.send(data))  
+    .catch(next);
+});
+
+router.get('/geocode', (req, res, next) => {
+  return mapbox.geocode(req.query.location)
+    .then(data => res.json(data))
     .catch(next);
 });
 
