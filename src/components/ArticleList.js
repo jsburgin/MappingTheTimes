c51e44164b6639b24a92e3731a4c819b9cc5eb0d
Sidney Packer <sidp@knights.ucf.edu>
2017-10-15 03:29:37 -0400

    Add scrolliosis

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,21 +3,27 @@ import _ from 'lodash';
 import Article from './Article';
 import ArticleListSlider from './ArticleListSlider';
 
-const ArticleList = props => {
-  const articles = []
-  for (let day of props.days) {
-    for (let article of day.articles) {
-      articles.push(article);
-    }
+class ArticleList extends Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.days !== this.props.days
   }
-  return (
-    <div className='article-list'>
-      <ArticleListSlider />
-      {
-      articles.map(a => <Article key={_.uniqueId('article:')} data={a} onChange={props.onChange} emitter={props.emitter} />)
+
+  render() {
+    const articles = []
+    for (let day of this.props.days) {
+      for (let article of day.articles) {
+        articles.push(article);
       }
-    </div>
-  );
-};
+    }
+    return (
+      <div className='article-list'>
+        <ArticleListSlider />
+        {
+        articles.map(a => <Article key={_.uniqueId('article:')} data={a} onChange={this.props.onChange} emitter={this.props.emitter} />)
+        }
+      </div>
+    );
+  }
+}
 
 export default ArticleList;
