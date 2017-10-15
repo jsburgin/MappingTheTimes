03793eb790243214cb77381c582cd9e946dfb07b
Joshua Burgin <jsburgin@crimson.ua.edu>
2017-10-15 05:53:15 -0400

    added static router to backend

--- backend/routes/config.js
+++ backend/routes/config.js
@@ -1,7 +1,9 @@
 const rootRouter = require('./routers/root');
 const apiRouter = require('./routers/api');
+const staticRouter = require('./routers/static');
 
 module.exports = (app) => {
   app.use('/api', apiRouter);
+  app.use('/static', staticRouter);
   app.use('/', rootRouter);
 };
