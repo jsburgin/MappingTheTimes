229c37675b92df7ea1061f8fa85fb58c596eaf20
Sidney Packer <sidp@knights.ucf.edu>
2017-10-15 04:39:12 -0400

    Start setting up header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Slider from 'rc-slider';
+import Tooltip from 'rc-tooltip';
+import 'rc-slider/assets/index.css';
 
-const Header = props => (
-  <div className='header'>
-    <h1>Mapping The Times</h1>
-  </div>
-);
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Handle = Slider.Handle;
 
-export default Header;
+const handle = (props) => {
+  const { value, dragging, index, ...restProps } = props;
+  return (
+    <Tooltip
+      prefixCls="rc-slider-tooltip"
+      overlay={value}
+      visible={dragging}
+      placement="top"
+      key={index}
+    >
+      <Handle value={value} {...restProps} />
+    </Tooltip>
+  );
+};
+
+export default class Header extends Component {
+  handleAfterChange = finishVal => {
+    console.log('finished changing');
+  }
+
+  render() {
+    return (
+      <div className='header'>
+        <h1>Mapping The Times</h1>
+        <Slider
+          min={0}
+          max={this.props.days.length}
+          defaultValue={this.props.days.length} 
+          handle={handle}
+          onAfterChange={this.handleAfterChange}
+        />
+      </div>
+    );
+  }
+}
